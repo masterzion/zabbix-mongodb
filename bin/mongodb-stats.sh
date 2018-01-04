3d94e579004f2bb8f60215de430bfeff6fc66137
masterzion <master.zion@gmail.com>
2018-01-04 17:02:11 +0100

    Update mongodb-stats.sh
    new config patch

--- bin/mongodb-stats.sh
+++ bin/mongodb-stats.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
 # Date:                 22/01/2017
 # Author:               Long Chen
 # Description:          A script to send MongoDB stats to zabbix server by using zabbix sender
 # Requires:             Zabbix Sender, zabbix-mongodb.py
 
+# Last update:          Jairo Moreno
+# Date:                 04/01/2018
+
 get_MongoDB_metrics(){
-python /usr/local/bin/zabbix-mongodb.py
+python /usr/local/bin/zabbix-mongodb.py 
 }
 
 # Send the results to zabbix server by using zabbix sender
-result=$(get_MongoDB_metrics | /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
+result=$(get_MongoDB_metrics | /bin/zabbix_sender  -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
 response=$(echo "$result" | awk -F ';' '$1 ~ /^info/ && match($1,/[0-9].*$/) {sum+=substr($1,RSTART,RLENGTH)} END {print sum}')
 if [ -n "$response" ]; then
         echo "$response"
