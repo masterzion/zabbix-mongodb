ec857d60f6151a79a65893431ec9711c464a1bc5
masterzion <master.zion@gmail.com>
2018-01-10 14:56:21 +0100

    Update mongodb-stats.sh
    added -s paramter

--- bin/mongodb-stats.sh
+++ bin/mongodb-stats.sh
@@ -7,12 +7,14 @@
 # Last update:          Jairo Moreno
 # Date:                 04/01/2018
 
+HOST_ALIAS=$HOSTNAME #it may change
+
 get_MongoDB_metrics(){
 python /usr/local/bin/zabbix-mongodb.py 
 }
 
 # Send the results to zabbix server by using zabbix sender
-result=$(get_MongoDB_metrics | /bin/zabbix_sender  -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
+result=$(get_MongoDB_metrics | /bin/zabbix_sender  -s $HOST_ALIAS -c /etc/zabbix/zabbix_agentd.conf -i - 2>&1)
 response=$(echo "$result" | awk -F ';' '$1 ~ /^info/ && match($1,/[0-9].*$/) {sum+=substr($1,RSTART,RLENGTH)} END {print sum}')
 if [ -n "$response" ]; then
         echo "$response"
